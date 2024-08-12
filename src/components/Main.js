3d3c79eaa57e609941203cddafa0781941c1e525
Nathan Ferdinando <nferdica@outlook.com>
2024-08-12 14:16:34 -0300

    feat: Creating components

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import "./Main.css";
+
+export default class Main extends Component {
+  state = {
+      novaTarefa: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+        <form action="#">
+          <input onChange={this.handleChange} type="text"/>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
